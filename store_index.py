16c89b8113a4cf9ed8a83d4e1691322a2a93c761
MennaHefny25 <mennahefny25@gmail.com>
2024-06-29 09:35:56 +0300

    store vectors in pinecone

--- store_index.py
+++ store_index.py
@@ -8,3 +8,11 @@ load_dotenv()
 PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
 
 
+extracted_data = load_pdf("./data")
+text_chunks = text_split(extracted_data=extracted_data)
+embeddings = download_hugging_face_embedding()
+
+index_name = "medical-chatbot"
+
+vectorstore = PineconeVectorStore(index_name=index_name, embedding=embeddings)
+vectorstore.from_texts([t.page_content for t in text_chunks], index_name=index_name, embedding=embeddings)

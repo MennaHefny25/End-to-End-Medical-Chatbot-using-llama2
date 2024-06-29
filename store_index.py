0984e494a066aa692fd14cd209adbb5b83b03f59
MennaHefny25 <mennahefny25@gmail.com>
2024-06-29 09:04:09 +0300

    storing vectors

---
+++ store_index.py
@@ -0,0 +1,10 @@
+from src.helper import *
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+
+

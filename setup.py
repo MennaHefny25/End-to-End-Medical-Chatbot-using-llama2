284720523892c3ae8047b8a33fd8c9d7af93950a
MennaHefny25 <mennahefny25@gmail.com>
2024-06-29 08:39:13 +0300

    create local package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Menna Hefny',
+    author_email= 'mennahefny25@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
